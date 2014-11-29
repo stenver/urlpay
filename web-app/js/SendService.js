c3c7f7f032cfb39ce41cf4b7f0f99308fb0e9ddc
Taivo Käsper <taivo.kasper@gmail.com>
2014-11-29 17:21:53 +0200

    Added form modification

--- web-app/js/SendService.js
+++ web-app/js/SendService.js
@@ -1,3 +1,7 @@
-app.service('SendService', function () {
+app.service('SendService', function ($resource) {
+    var TransferResource = $resource('/app/transfer');
 
+    this.transfer = function (data, successFn, errorFn) {
+        return TransferResource.save(data, successFn, errorFn);
+    };
 });
