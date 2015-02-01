3efecba1979524287d9e6efccaa606f33a6c3be5
Kristjan-Julius Laak <julius.laak@gmail.com>
2015-02-01 17:42:11 +0200

    js validation

--- web-app/js/plugins.js
+++ web-app/js/plugins.js
@@ -23,20 +23,29 @@
 
 // Place any jQuery/helper plugins in here.
 
+// placeholder hack
 // ref: http://diveintohtml5.org/detect.html
 function supports_input_placeholder()
 {
 var i = document.createElement('input');
 return 'placeholder' in i;
 }
-
 if(!supports_input_placeholder()) {
-var fields = document.getElementsByTagName('INPUT');
-for(var i=0; i < fields.length; i++) {
-  if(fields[i].hasAttribute('placeholder')) {
-    fields[i].defaultValue = fields[i].getAttribute('placeholder');
-    fields[i].onfocus = function() { if(this.value == this.defaultValue) this.value = ''; }
-    fields[i].onblur = function() { if(this.value == '') this.value = this.defaultValue; }
-  }
+    var fields = document.getElementsByTagName('INPUT');
+    for(var i=0; i < fields.length; i++) {
+      if(fields[i].hasAttribute('placeholder')) {
+        fields[i].defaultValue = fields[i].getAttribute('placeholder');
+        fields[i].onfocus = function() { if(this.value == this.defaultValue) this.value = ''; }
+        fields[i].onblur = function() { if(this.value == '') this.value = this.defaultValue; }
+      }
+    }
 }
+
+// js form validation
+function validateForm() {
+    var x = document.forms["receiveForm"]["receiveInputIban"].value;
+    if (x==null || x=="") {
+        alert("IBAN must be filled out");
+        return false;
+    }
 }
